c838a5f61de2e99ea4401b06f4aee9158977f639
Jagoda <jagodadawidowska123@gmail.com>
2022-08-02 01:17:36 +0200

    issue
    refactoring

--- src/main/java/com/jdawidowska/equipmentRentalService/data/repos/RentedInventoryRepository.java
+++ src/main/java/com/jdawidowska/equipmentRentalService/data/repos/RentedInventoryRepository.java
@@ -1,10 +1,10 @@
 package com.jdawidowska.equipmentRentalService.data.repos;
 
-import com.jdawidowska.equipmentRentalService.data.entities.rentedInventory;
+import com.jdawidowska.equipmentRentalService.data.entities.RentedInventory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RentedRepository extends CrudRepository<rentedInventory, Long>{
+public interface RentedInventoryRepository extends CrudRepository<RentedInventory, Long>{
 
 }
