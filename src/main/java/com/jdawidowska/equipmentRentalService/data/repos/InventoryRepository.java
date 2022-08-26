b5b9f3b6ca6a0f5d069de3084da95778f1e1b176
JagodaDawidowska <jagodadawidowska123@gmail.com>
2022-08-26 12:41:10 +0200

    functions add and remove equpment in inventory added.

--- src/main/java/com/jdawidowska/equipmentRentalService/data/repos/InventoryRepository.java
+++ src/main/java/com/jdawidowska/equipmentRentalService/data/repos/InventoryRepository.java
@@ -1,11 +1,16 @@
 package com.jdawidowska.equipmentRentalService.data.repos;
 
 import com.jdawidowska.equipmentRentalService.data.entities.Inventory;
+import com.jdawidowska.equipmentRentalService.data.entities.RentedInventory;
+import com.jdawidowska.equipmentRentalService.model.EquipmentEnum;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InventoryRepository extends CrudRepository<Inventory, Long>{
--- src/main/java/com/jdawidowska/equipmentRentalService/services/InventoryService.java
+++ src/main/java/com/jdawidowska/equipmentRentalService/services/InventoryService.java
@@ -1,8 +1,17 @@
 package com.jdawidowska.equipmentRentalService.services;
 
+import com.jdawidowska.equipmentRentalService.api.dto.request.InventoryRequest;
 import com.jdawidowska.equipmentRentalService.data.entities.Inventory;
 import com.jdawidowska.equipmentRentalService.data.repos.InventoryRepository;
+import org.springframework.data.util.Optionals;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class InventoryService {
@@ -16,5 +25,21 @@ public class InventoryService {
         return inventoryRepository.findAll();
     }
 
+    public boolean add(InventoryRequest inventoryRequest) {
+      Inventory inventory = new Inventory();
+      inventory.setItemName(inventoryRequest.getItemName());
+      inventory.setTotalAmount(inventoryRequest.getTotalAmount());
+      inventory.setAvailableAmount(inventoryRequest.getAvailableAmount());
+      inventoryRepository.save(inventory);
+      return true;
+    }
 
+    public boolean remove(InventoryRequest inventoryRequest){
+        Inventory inventory = inventoryRepository.findById(InventoryRequest.getIdItem()).orElse(null);
+        if(inventory !=null) {
+            inventoryRepository.deleteById(inventory.getId());
+            return true;
+        }else return false;
+    }
 }
+
