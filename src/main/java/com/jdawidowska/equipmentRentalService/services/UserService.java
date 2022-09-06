52c2d50e25a9efd5b53e455ed3ff4881db99ef86
JagodaDawidowska <jagodadawidowska123@gmail.com>
2022-09-06 12:49:26 +0200

    added protection in register function for alredy exists e-mail (UserService).

--- src/main/java/com/jdawidowska/equipmentRentalService/services/UserService.java
+++ src/main/java/com/jdawidowska/equipmentRentalService/services/UserService.java
@@ -7,20 +7,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public void addUser(UserRequest userRequest){
+    public boolean addUser(UserRequest userRequest){
         User user = new User();
         user.setName(userRequest.getName());
         user.setSurname(userRequest.getSurname());
         user.setEmail(userRequest.getEmail());
         user.setPassword(userRequest.getPassword());
         user.setRole(userRequest.getRole());
-        userRepository.save(user);
+
+        if(!userRepository.existsByEmail(userRequest.getEmail())){
+            userRepository.save(user);
+            return true;
+        }else return false;
     }
 }
